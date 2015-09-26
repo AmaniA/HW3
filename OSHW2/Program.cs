c2aa84660f95d26416ce7d5f8bead294407a9fd9
AmaniA <aalmatrafi@luc.edu>
2015-09-25 21:51:56 -0500

    Signed-off-by: AmaniA <aalmatrafi@luc.edu>

---
+++ OSHW2/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace FileCopy
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var bufferSize = int.Parse(args[0]);
+            var sourceFile = new FileInfo(args[1]);
+            var destFile = new FileInfo(args[2]);
+            var start = DateTime.Now;
+            CopyFile(sourceFile, destFile, bufferSize);
+            var finish = DateTime.Now;
+            var totalMilliSeconds = (finish - start).TotalMilliseconds;
+            var fileSize = sourceFile.Length;
+            var bytesPerMilliSecond = fileSize / totalMilliSeconds;
+            var bytesPerSeconds = bytesPerMilliSecond / 1000;
+         }
+
+        private static void CopyFile(FileInfo sourceFile, FileInfo destFile, int bufferSize)
+        {
+            using (var source = OpenSourceFile(sourceFile))
+            {
+                using (var dest = OpenDestFile(destFile))
+                {
+                    CopyData(source, dest, bufferSize);
+                }
+            }
+        }
+
+        private static void CopyData(Stream source, Stream dest, int bufferSize)
+        {
+            var buffer = new byte[bufferSize];
+            int readLen;
+          
+            while ((readLen =source.Read(buffer, 0, buffer.Length))!=0 )
+            {
+                dest.Write(buffer, 0, readLen);
+            }
+        }
+
+        private static Stream OpenSourceFile(FileInfo file)
+        {
+            return new FileStream(file.FullName, FileMode.Open, FileAccess.Read);
+        }
+
+        private static Stream OpenDestFile(FileInfo file)
+        {
+            return new FileStream(file.FullName, FileMode.Create, FileAccess.Write);
+        }
+
+    }
+}
