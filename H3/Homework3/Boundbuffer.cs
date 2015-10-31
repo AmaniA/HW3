324819e452a774bb6f78a0b7dd352a4630664704
AmaniA <aalmatrafi@luc.edu>
2015-10-30 20:05:18 -0500

    c

---
+++ H3/Homework3/Boundbuffer.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Threading;
+using System.Collections.Generic;
+
+namespace Homework3
+{
+    internal class Boundbuffer<T>
+    {
+ 
+            
+        private Queue<T> _queue = new Queue<T>();
+        private Semaphore _empty = new Semaphore(4000001, 4000001);
+        private Semaphore _full = new Semaphore(0, 4000001);
+        private SpinLock _lock = new SpinLock();
+       
+      
+
+        public void Enqueue(T item)
+        {
+          
+            _empty.WaitOne();
+            var taken = false;
+            _lock.Enter(ref taken);
+            _queue.Enqueue(item);
+            _lock.Exit();
+            _full.Release();
+        }
+
+        public T Dequeue()
+        {
+            _full.WaitOne();
+            var taken = false;
+            _lock.Enter(ref taken);
+            var item = _queue.Dequeue();
+            _lock.Exit();
+            _empty.Release();
+            return item;
+        }
+    }
+}
---
+++ H3/Homework3/NumberReader.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Diagnostics;
+
+namespace Homework3 {
+  
+    class NumberReader : IDisposable {
+        private readonly TextReader _reader;
+
+       
+        public NumberReader(FileInfo file) {
+            Debug.WriteLine("I'm here 1 ");
+            _reader = new StreamReader(new BufferedStream(new FileStream(
+                file.FullName, FileMode.Open, FileAccess.Read, FileShare.Read, 4096, FileOptions.SequentialScan), 65536));
+           
+        }
+
+        public IEnumerable<long> ReadIntegers() {
+            string line;
+            while ((line = _reader.ReadLine()) != null) {
+                var value = long.Parse(line);
+                yield return value;
+
+            }
+        } 
+        
+        public void Dispose() {
+            _reader.Dispose();
+        }
+    
+    }
+}
